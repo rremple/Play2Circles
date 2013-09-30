6f5b75586ef1694de63f7a84bc4a01792d14e112
Russ Remple <russ.remple@gmail.com>
2013-09-29 21:42:50 -0700

    remove cloudbees sbt plugin (old version of Scala)

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,7 @@
 import sbt._
 import Keys._
 import play.Project._
+//import cloudbees.Plugin._
 
 object ApplicationBuild extends Build {
 
@@ -13,9 +14,8 @@ object ApplicationBuild extends Build {
     anorm
   )
 
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+  val main = play.Project(appName, appVersion, appDependencies)
+//    .settings(cloudBeesSettings :_*)
+//    .settings(CloudBees.applicationId := Some("your account name/your app name"))
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,3 +6,9 @@ resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/release
 
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.0")
+
+//Cloudbees
+//resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
+//
+//addSbtPlugin("com.cloudbees.deploy.play" % "sbt-cloudbees-play-plugin" % "0.5-SNAPSHOT")
+
